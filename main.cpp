80636c43c7cf1a3c3f0dce7e1ea7bf149768ded2
GiovanyH <56926698+GiovanyH@users.noreply.github.com>
2024-03-08 21:31:24 -0300

    Removed README.md

--- main.cpp
+++ main.cpp
@@ -1,29 +1,3 @@
-/*
-	Add this to READ.me later
-
-	Project made by GiovanyH
-	
-	This is going to be a Horror Game Engine for my horror game,
-	it`s going to be made using C/C++, using almost zero libraries,
-	only the ones that are really necessary, like OpenGL, GLFW, etc.
-
-	For now, I`m going to use OpenGL and GLFW to make the window and
-	the rendering, but I`m going to try to make my own rendering system
-	later on.
-
-	I will also need some stuff that I used to use with glm
-	like vectors, matrices, etc. I`m going to make my own
-
-	All of that stuff is going to be inside of core/types/types.h,
-	the reason why types.h is inside of a folder called types is because
-	I plan on having more headers inside of the types folder
-
-	When I use GLFW for Input, I`m gonna be renaming them to my own
-	naming convention, so I can have a better understanding of what I`m using
-*/
-
-// naming convention: gioVec2
-
 #include <iostream>
 
 // including types.h
