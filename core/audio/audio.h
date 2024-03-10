1f41ca3767989f9e4866ec0eae9faac368de49c5
includeGiovany <harbsgiovany@gmail.com>
2024-03-10 12:05:58 -0300

    Playing Audio

--- core/audio/audio.h
+++ core/audio/audio.h
@@ -1,127 +1,8 @@
-// naming convention: gioVec2
-
-/*
-	Next step is to add ImGui to the project
-	in order to easily output the text to the screen
-	instead of printing it all the time.
-
-	Next, I want to have a console for the engine
-	where we can see the output of the engine's log
-	without having to have a console window.
-*/
-
-// ImGui designs are going to be in a separate file
-// called "imgui_designs.h", inside of the "editor" folder, for now
-
-/*
-#include <iostream>
-
-// including glad
-#include "core/libs/glad/glad.h"
-
-// including types.h
-#include "core/types/types.h"
-
-// including OS.h
-#include "core/OS/OS.h"
-
-// including imgui_designs
-#include "editor/imgui_designs.h"
-
-// Layout of the application/game
-void Init()
-{
-	// Initialize the game
-	// No need for fancy stuff
-	gladLoadGL();
-
-	gioImGui::Init();
-}
-
-void Update()
-{
-	// Update the game
-	gioImGui::Update();
-}
-
-void Render()
-{
-	// Render the game
-}
-
-void CleanUp()
-{
-	// Clean up the game
-
-	gioImGui::CleanUp();
-}
-
-int main()
-{
-	AddSetting("hello", (void*)"world");
-	std::cout << (const char*)EngineConfig["hello"] << std::endl;
-
-	// creating a gioWindow
-	gioWindow *window = new gioWindow(800, 600, "Gio Engine");
-
-	AddSetting("OS-window", window);
-
-	// Begin
-	Init();
-
-	gioInput input;
-
-	// Game loop
-	while (!window->ShouldClose())
-	{
-		// Update window
-
-		// Update
-		Update();
-
-		// Render
-		Render();
-
-		// print the gioInput
-		input.Update(window->window);
-
-		gioInput *inputPtr = new gioInput;
-
-		if (input.key != -1 && input.key == GLFW_KEY_D && GetKeyboardInput("gio_left") == nullptr)
-		{
-			std::cout << "Key: " << input.key << std::endl;
-			inputPtr->key = input.key;
-			AddKeyboardInput("gio_left", &inputPtr->key);
-		}
-
-		if (GetKeyboardInput("gio_left") != nullptr)
-		{
-			std::cout << "Key: " << *GetKeyboardInput("gio_left") << std::endl;
-		}
-
-		// Add the input to the settings
-		AddSetting("OS-keyboardinput-string", (void*)gioGetKeyboardInputs(input));
-		AddSetting("OS-mouseinput-string", (void*)gioGetMouseInputs(input));
-		AddSetting("OS-gamepadinput-string", (void*)gioGetGamepadInputs(input));
-
-		// Swap buffers
-		window->SwapBuffers();
-		// Poll events
-		window->PollEvents();
-	}
-	CleanUp();
-
-
-	return 0;
-}
-*/
-
-// testing some audio code
 #pragma once
 
 /*
-    in this file, audio.h, I'll be using
-    OpenAL to handle the audio of the engine.
+	in this file, audio.h, I'll be using
+	OpenAL to handle the audio of the engine.
 */
 
 #include <AL/al.h>
@@ -161,8 +42,8 @@ int main()
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "core/audio/alhelpers.h"
-#include "core/audio/win_main_utf8.h"
+#include "alhelpers.h"
+#include "win_main_utf8.h"
 
 // include sndfile
 #include <sndfile.h>
